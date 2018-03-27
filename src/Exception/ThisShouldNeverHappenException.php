890379066d71b84845cd7f6e2dcb6151acab165c
Jiri Travnicek <jiri.travnicek@outlook.com>
2018-03-27 12:07:50 +0200

    ThisShouldNeverHappenException: append "This should never happen!"

--- src/Exception/ThisShouldNeverHappenException.php
+++ src/Exception/ThisShouldNeverHappenException.php
@@ -8,6 +8,7 @@ class ThisShouldNeverHappenException extends \LogicException
 {
     public function __construct($message = '', Throwable $previous = null)
     {
+        $message .= '. This should never happen!';
         parent::__construct($message, 0, $previous);
     }
 }
