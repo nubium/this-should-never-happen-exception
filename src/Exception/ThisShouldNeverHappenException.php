0897f2031a3db45bb0aa5c8da17ac109dde709b2
Jan Tykal <jan.tykal@nubium.cz>
2018-06-08 10:22:46 +0200

    ThisShouldNeverHappenException: fixed message formating

--- src/Exception/ThisShouldNeverHappenException.php
+++ src/Exception/ThisShouldNeverHappenException.php
@@ -6,9 +6,12 @@ use Throwable;
 
 class ThisShouldNeverHappenException extends \LogicException
 {
-    public function __construct($message = '', Throwable $previous = null)
+    public function __construct(string $message = '', Throwable $previous = null)
     {
-        $message .= '. This should never happen!';
+        if ($message !== '') {
+            $message = rtrim($message, '.') . '. ';
+        }
+        $message .= 'This should never happen!';
         parent::__construct($message, 0, $previous);
     }
 }
